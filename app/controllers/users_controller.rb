903f09ec87a8c055d9a2754bc323d723f6f50214
takamori nambu <roadfox303@gmail.com>
2019-11-22 16:11:39 +0900

    全 quit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,16 +10,19 @@ class UsersController < ApplicationController
     @page_name = "User Profile"
     gon.page_name = @page_name
     @user = User.find(params[:id])
-    @blogs_all = Blog.where(user_id: current_user.id).order(created_at: :desc)
+    @blogs_all = Blog.where(user_id: @user.id).order(created_at: :desc)
     @blogs10 = @blogs_all.limit(10)
     @blogs_num = @blogs_all.size
     @nice_total = 0
     @blogs_all.each do |blog|
       @nice_total += blog.nices.size
     end
+    @nices = Nice.where(user_id: @user.id).order(created_at: :desc).limit(10)
   end
   def phrases
     @page_name = "User Phrase List"
+    @user = User.find(params[:id])
+    @blogs = Blog.where(user_id: @user.id).order(created_at: :desc)
   end
   def nices
     @page_name = "Nice List"
