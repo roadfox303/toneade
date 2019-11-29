454cd618d3e4d2f99a44a6492726b6120bfe9f65
takamori nambu <roadfox303@gmail.com>
2019-11-29 17:26:30 +0900

    Rspec User、Blogモデルバリデーションのテスト

--- Gemfile
+++ Gemfile
@@ -29,19 +29,26 @@ group :development, :test do
   gem 'pry-rails'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'spring'
+
 end
 
 group :development do
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
-  gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 
   gem 'letter_opener_web'
 end
 
 group :test do
+  gem 'rspec-rails'
+  gem 'spring-commands-rspec'
+  gem 'factory_bot_rails'
+  gem 'faker'
   gem 'capybara', '>= 2.15'
+  gem 'database_cleaner'
+  gem 'launchy'
   gem 'selenium-webdriver'
   gem 'chromedriver-helper'
 end
---
+++ spec/models/blog_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Blog, type: :model do
+
+  before do
+    @user = create(:user)
+  end
+
+  it "ブログが作成成功" do
+    Blog.create(title: 'あああ', content: 'あいうえお', user_id: @user.id)
+    Blog.create(title: 'abcde', content: '', user_id: @user.id)
+    expect(Blog.all.count).to eq 2
+  end
+
+  it "ブログが作成失敗" do
+    Blog.create(title: '', content: 'あいうえお', user_id: @user.id)
+    Blog.create(title: [*1..100].join, content: '', user_id: @user.id)
+    Blog.create(title: 'タイトル', content: 'あいうえお', user_id: '')
+    expect(Blog.all.count).to eq 0
+  end
+
+end
