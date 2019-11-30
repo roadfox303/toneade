ea7ef687b8776bf9d3c1a1127805ed8e53129b58
takamori nambu <roadfox303@gmail.com>
2019-11-30 15:47:10 +0900

    シードの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 30.times do |n|
-  User.create!(name: "テストさん#{n}" ,
-               email: "test#{n}@gmail.com",
+  User.create(name: "テストさん#{n+1}" ,
+               email: "test#{n+1}@gmail.com",
                password: "password"
                )
 end
 
 20.times do |n|
-  Blog.create!(title: "シード#{n}" ,
-               content: "シードコメント#{n}",
-               user_id: User.find(n).id
-               )
+  Blog.create(title: "シード#{n+1}" ,
+              content: "シードコメント#{n+1}",
+              user_id: User.find(n+1).id,
+              phrase_attributes: {
+                user_id: User.find(n+1).id,
+                key: 1,
+                scale: 1,
+                bpm: 120,
+                master_data: ""
+              }
+
+              )
 end
