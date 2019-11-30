5c8e89f250d44db5aaef6d11aa906e672ec94c05
takamori nambu <roadfox303@gmail.com>
2019-11-30 16:37:43 +0900

    タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Toneade
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
