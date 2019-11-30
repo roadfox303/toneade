5340a66b9f8bd97318b15e270c4243f25849ee5e
takamori nambu <roadfox303@gmail.com>
2019-11-30 14:38:45 +0900

    α1.0 課題での指摘訂正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
+
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -50,7 +51,7 @@ Rails.application.configure do
 
   config.assets.paths << Rails.root.join("app", "assets", "fonts")
   config.assets.precompile += %w( .svg .eot .woff .ttf )
-  
+
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
