cd1cf51f1646f0f03a1d0ea6084f3add0d59a48f
takamori nambu <roadfox303@gmail.com>
2020-03-06 17:57:39 +0900

    optional packages view実装

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,5 +1,7 @@
 class PackagesController < ApplicationController
   def index
+    @page_name = 'Optional Packages'
+    gon.page_name = @page_name
     @items = Stripe::SKU.list().data
     # @patchs = Stripe::Product.list().data.find_all { |product|
     #   product.metadata.category == "patch"
@@ -12,22 +14,22 @@ class PackagesController < ApplicationController
 
   end
   def create
-
-  @amount = 500
-  customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source  => params[:stripeToken]
-  )
-
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
-
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
-end
+    
+#   @amount = 500
+#   customer = Stripe::Customer.create(
+#     :email => params[:stripeEmail],
+#     :source  => params[:stripeToken]
+#   )
+#
+#   charge = Stripe::Charge.create(
+#     :customer    => customer.id,
+#     :amount      => @amount,
+#     :description => 'Rails Stripe customer',
+#     :currency    => 'usd'
+#   )
+#
+# rescue Stripe::CardError => e
+#   flash[:error] = e.message
+#   redirect_to new_charge_path
+  end
 end
