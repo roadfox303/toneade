7519162953f016b26de5e75a8bdbfb404a6c33c3
takamori nambu <roadfox303@gmail.com>
2020-03-08 20:21:18 +0900

    stripe webhook test6

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,46 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  require 'json'
   get 'relationships/create'
   get 'relationships/destroy'
   get 'users/show'
   root 'blogs#top'
+
+  # Using Sinatra
+  post 'packages/webhook' do
+    payload = request.body.read
+    event = nil
+
+    begin
+      event = Stripe::Event.construct_from(
+        JSON.parse(payload, symbolize_names: true)
+      )
+    rescue JSON::ParserError => e
+      # Invalid payload
+      status 400
+      return
+    end
+
+    # Handle the event
+    case event.type
+    when 'payment_intent.succeeded'
+      payment_intent = event.data.object # contains a Stripe::PaymentIntent
+      # Then define and call a method to handle the successful payment intent.
+      # handle_payment_intent_succeeded(payment_intent)
+    when 'payment_method.attached'
+      payment_method = event.data.object # contains a Stripe::PaymentMethod
+      # Then define and call a method to handle the successful attachment of a PaymentMethod.
+      # handle_payment_method_attached(payment_method)
+    # ... handle other event types
+    else
+      # Unexpected event type
+      status 400
+      return
+    end
+
+    status 200
+  end
 
   # ログイン、アカウント編集後、任意のページに推移させるための記述
   # devise_for :users, controllers: {
@@ -35,6 +71,6 @@ Rails.application.routes.draw do
       get :success
     end
   end
-  
+
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
