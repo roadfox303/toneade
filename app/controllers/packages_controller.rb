b984797251d38c53004414aaee2aff041e105893
takamori nambu <roadfox303@gmail.com>
2020-03-08 18:59:44 +0900

    stripe webhook テスト2

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -15,7 +15,41 @@ class PackagesController < ApplicationController
   end
 
   def create
-    logger.debug("stripe_webhook: #{params}")
+    require 'json'
+    # Using Sinatra
+    post '/webhook' do
+      payload = request.body.read
+      event = nil
+
+      begin
+        event = Stripe::Event.construct_from(
+          JSON.parse(payload, symbolize_names: true)
+        )
+      rescue JSON::ParserError => e
+        # Invalid payload
+        status 400
+        return
+      end
+
+      # Handle the event
+      case event.type
+      when 'payment_intent.succeeded'
+        payment_intent = event.data.object # contains a Stripe::PaymentIntent
+        # Then define and call a method to handle the successful payment intent.
+        # handle_payment_intent_succeeded(payment_intent)
+      when 'payment_method.attached'
+        payment_method = event.data.object # contains a Stripe::PaymentMethod
+        # Then define and call a method to handle the successful attachment of a PaymentMethod.
+        # handle_payment_method_attached(payment_method)
+      # ... handle other event types
+      else
+        # Unexpected event type
+        status 400
+        return
+      end
+
+      status 200
+    end
   end
 
   def destroy
